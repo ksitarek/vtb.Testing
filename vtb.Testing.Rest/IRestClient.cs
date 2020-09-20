b09f59a2759f0942c477c8468cd7002cfb332870
Krystian Sitarek <krystiansitarek@gmail.com>
2020-09-20 20:00:41 +0200

    Hello

---
+++ vtb.Testing.Rest/IRestClient.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using RESTFulSense.Clients;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace vtb.Testing.Rest
+{
+    public interface IRestClient : IRESTFulApiFactoryClient
+    {
+        HttpRequestHeaders DefaultRequestHeaders { get; }
+
+        ValueTask<TResponse> PostAsync<TRequest, TResponse>(string relativeUrl, TRequest content);
+
+        ValueTask PostAsync<TRequest>(string relativeUrl, TRequest content);
+
+        ValueTask PatchAsync<TRequest>(string relativeUrl,
+            JsonPatchDocument<TRequest> content) where TRequest : class;
+    }
+}
